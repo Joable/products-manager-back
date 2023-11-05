d9d941aea3491b679740842c0357e0544466955d
Joaquin <jaltable@fi.uba.ar>
2023-11-05 14:47:34 -0300

    created 'products' route

---
+++ routes/products.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', );
+
+router.get('/:id', );
+
+router.post('/', );
+
+router.put('/id', );
+
+router.delete('/:id', );
+
+module.exports = router;
