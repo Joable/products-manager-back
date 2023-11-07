3f1c4ba73bd3bf80ca5e6113a471570d50bc4b44
Joaquin <jaltable@fi.uba.ar>
2023-11-07 17:54:06 -0300

    now the 'productsModel' has te 'img' property

--- models/productsModel.js
+++ models/productsModel.js
@@ -2,7 +2,8 @@ const mongoose = require("../config/mongodb");
 
 const productsSchema = mongoose.Schema({
     name: String,
-    price: Number
+    price: String,
+    img: String
 });
 
 module.exports = mongoose.model("products", productsSchema);
