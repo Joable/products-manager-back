394fe9ceecf7d103d74f58299506b065418ea4ad
Joaquin <jaltable@fi.uba.ar>
2023-11-13 17:46:17 -0300

    added requirements to the schemaType

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,8 +1,17 @@
 const mongoose = require("../config/mongodb");
 
 const productsSchema = mongoose.Schema({
-    name: String,
-    price: String,
+    name: {
+        type: String,
+        require: [true, 'This field is requiered.'],
+        minLength: [3, 'The name of the product must be at least 3 characters long.'],
+        maxLength: [100, 'This field may not exceed 100 characters long.']
+    },
+    price: {
+        type: String,
+        require: [true, 'This field is requiered.'],
+        maxLength: [10, 'This field may not exceed 10 characters long.']
+    },
     img: String
 });
 
--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,7 @@ router.get('/:id', productController.getById);
 
 router.post('/', productController.create);
 
-router.put('/id', productController.update);
+router.put('/:id', productController.update);
 
 router.delete('/:id', productController.delete);
 
